2fda5b555d1ae03dc767def06efa41d6b3f35172
TaddOlson <jonathantaddolson95@gmail.com>
2023-12-28 22:25:48 -0600

    Set up player movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or prive reference
+    // data type (int >whole numbers<, float >decimal<, bool >true or false statements<, string >letters, phrase, or statements<) 
+    //every variable has a name 
+    // optional value assigned
+    private float _speed = 3.5f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new positon (0, 0 , 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+                    //new Vector3(1, 0, 0) * 5 * real time
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+         
+    }
+}
