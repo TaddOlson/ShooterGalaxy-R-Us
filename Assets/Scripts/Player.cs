b6ef75fc4880b9501c8bb44966509c1353d0855d
TaddOlson <jonathantaddolson95@gmail.com>
2023-12-29 23:14:37 -0600

    Started working on player bounds.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     // data type (int >whole numbers<, float >decimal<, bool >true or false statements<, string >letters, phrase, or statements<) 
     //every variable has a name 
     // optional value assigned
+    [SerializeField]
     private float _speed = 3.5f; 
 
     // Start is called before the first frame update
@@ -23,10 +24,26 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-                    //new Vector3(1, 0, 0) * 5 * real time
+        //new Vector3(1, 0, 0) * 0 * 3.5f * real time
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
-         
+        if (transform.position.y >= 5.9f)
+        {
+            transform.position = new Vector3(transform.position.x, 5.9f, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-9.1f, transform.position.y, 0);
+        }
+        
+        // if palyer on the x > 11
+        // x pos = -11
+        // else if player on the x is less than -11
+        // x pos = 11
     }
 }
