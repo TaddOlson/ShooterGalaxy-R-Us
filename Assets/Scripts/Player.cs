b619ed35d672f94046ebddb88b7c7bef91e927fb
TaddOlson <jonathantaddolson95@gmail.com>
2023-12-31 11:03:34 -0600

    finished off making the how far the player can go on the screen.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,29 +21,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        //new Vector3(1, 0, 0) * 0 * 3.5f * real time
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+        
+        transform.Translate(movement * _speed * Time.deltaTime);
 
-        if (transform.position.y >= 5.9f)
-        {
-            transform.position = new Vector3(transform.position.x, 5.9f, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 5.9f), 0);
+
         if (transform.position.x >= 11.3f)
         {
-            transform.position = new Vector3(-9.1f, transform.position.y, 0);
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-        
-        // if palyer on the x > 11
-        // x pos = -11
-        // else if player on the x is less than -11
-        // x pos = 11
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+
     }
 }
