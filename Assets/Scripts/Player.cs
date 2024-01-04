7963f5c1f64416d3e2494470abc27499b228e28a
TaddOlson <jonathantaddolson95@gmail.com>
2024-01-03 20:36:06 -0600

    Put in a CD system for the fire rate.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,9 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
-   
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,8 +31,9 @@ public class Player : MonoBehaviour
         //if I hit the space key
         //spawn gameobject
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
+            _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0) , Quaternion.identity);
             
         }
